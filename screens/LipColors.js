67aa0074d9790ce29a8fe685a27da4ac7b5ee442
Chris Castro <home@castrombpro.home>
2017-08-14 06:15:07 -0400

    Optimize List Rendering

--- screens/LipColors.js
+++ screens/LipColors.js
@@ -32,8 +32,10 @@ const screenPadding = 15
 const screenPaddingHorizontal =  2*screenPadding
 const ColorCard = props => {
   return (
-    <View style={{width:screen.width,height:160,backgroundColor:Colors.pinkly,paddingVertical:2,paddingHorizontal:4,marginVertical:10}}>
-      <View style={{flex:1,justifyContent:'center',alignItems:'flex-end'}}>
+    <View style={{width:screen.width,height:160,backgroundColor:props.rgb,
+        paddingVertical:2,paddingHorizontal:4,marginVertical:0}}>
+      <View style={{flex:1,justifyContent:'space-between',alignItems:'center',flexDirection:'row'}}>
+        <FontPoiret text={props.rgb === Colors.purpleText ? 'could not load proper color' : ''} size={medium} color={Colors.white}/>
         <FontPoiret text={props.status === 'CURRENT' ? 'permanent collection' : props.status === 'LIMITEDEDITION' ? 'limited edition' : 'discontinued but still around'} size={medium} color={Colors.white}/>
       </View>
       <View style={{flex:3,...Views.middleNoFlex}}>
@@ -56,7 +58,10 @@ class LipColors extends Component {
     modalContent: {},
     user: this.props.user,
     colors: null,
-    colorsByFamily: null
+    shades: [],
+    colorsArray: null,
+    colorsByFamily: null,
+    isListReady: false
   }
 
   shouldComponentUpdate(nextProps,nextState){
@@ -73,19 +78,36 @@ class LipColors extends Component {
     if (newProps && newProps.getColors && newProps.getColors.allColors) {
       if (newProps.getColors.allColors !== this.state.colors) {
         let colors = newProps.getColors.allColors
-        let colorsByFamily = {
-          neutrals:colors.filter(color => color.family === "NEUTRALS"),
-          reds:colors.filter(color => color.family === "REDS"),
-          pinks:colors.filter(color => color.family === "PINKS"),
-          boldPinks:colors.filter(color => color.family === "BOLDPINKS"),
-          browns:colors.filter(color => color.family === "BROWNS"),
-          purples:colors.filter(color => color.family === "PURPLES"),
-          berries:colors.filter(color => color.family === "BERRIES"),
-          oranges:colors.filter(color => color.family === "ORANGES")
-        }
-        this.setState({colors,colorsByFamily},()=>{
-          console.log('colors',this.state.colors[0]);
-          console.log('filteredColors',this.state.colorsByFamily[0])
+        let shades = []
+        colors.forEach(color => {
+          this.setState({
+            [`${color.id}`]:color
+          },()=>{
+            shades.push(color.id)
+          })
+        })
+        // let colorsByFamily = {
+        //   neutrals:colors.filter(color => color.family === "NEUTRALS"),
+        //   reds:colors.filter(color => color.family === "REDS"),
+        //   pinks:colors.filter(color => color.family === "PINKS"),
+        //   boldPinks:colors.filter(color => color.family === "BOLDPINKS"),
+        //   browns:colors.filter(color => color.family === "BROWNS"),
+        //   purples:colors.filter(color => color.family === "PURPLES"),
+        //   berries:colors.filter(color => color.family === "BERRIES"),
+        //   oranges:colors.filter(color => color.family === "ORANGES")
+        // }
+        this.setState({shades},()=>{
+          this.setState({isListReady:true})
+
+          // let colorsArray = []
+          // this.state.shades.forEach(colorId => {
+          //   colorsArray.push(this.state[`${colorId}`])
+          // })
+          // this.setState({colorsArray})
+
+          // console.log('colors',this.state[`${this.state.shades[39]}`])
+          // console.log('shades length',this.state.shades.length);
+          // console.log(this.state);
         })
       }
     }
@@ -118,17 +140,25 @@ class LipColors extends Component {
     )
   }
 
+  renderColorsList(){
+    if (!this.state.isListReady) {
+      return (
+        <Text>Loading...</Text>
+      )
+    } else {
+      return this.state.shades.map(colorId => {
+        let color = this.state[`${colorId}`]
+        return <ColorCard key={color.id} family={color.family} tone={color.tone} name={color.name} rgb={color.rgb ? `rgb(${color.rgb})` : Colors.purpleText} finish={color.finish} status={color.status}/>
+      })
+    }
+  }
+
   renderMainContent(){
     return (
       <View style={{flex:1}}>
         <ScrollView
           contentContainerStyle={{marginBottom:56}}>
-            {
-              this.state.colors !== null ?
-              this.state.colors.map(color => {
-                return <ColorCard key={color.id} family={color.family} tone={color.tone} name={color.name} finish={color.finish} status={color.status}/>
-              }) : <Text>Loading...</Text>
-            }
+            {this.renderColorsList()}
         </ScrollView>
       </View>
     )
