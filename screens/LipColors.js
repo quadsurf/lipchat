dac743cb187ba65a82da2b7b5168d4f20f4ce802
Chris Castro <home@castrombpro.home>
2017-08-15 23:03:40 -0400

    Sort Colors by Family

--- screens/LipColors.js
+++ screens/LipColors.js
@@ -11,6 +11,7 @@ import {
 //LIBS
 import { compose,graphql } from 'react-apollo'
 import { Ionicons } from '@expo/vector-icons'
+import { DotsLoader } from 'react-native-indicator'
 
 // GQL
 import { GetColorsAndInventories } from '../api/db/queries'
@@ -32,7 +33,7 @@ const screen = getDimensions()
 const vspace = 10
 const screenPadding = 15
 const screenPaddingHorizontal =  2*screenPadding
-
+const inventoryError = 'updating your inventory'
 const ColorCard = props => {
   return (
     <View style={{width:screen.width,height:170,backgroundColor:props.rgb,
@@ -41,15 +42,18 @@ const ColorCard = props => {
         <FontPoiret text={props.rgb === Colors.purpleText ? 'could not load proper color' : ''} size={medium} color={Colors.white}/>
         <FontPoiret text={props.status === 'CURRENT' ? 'main collection' : props.status === 'LIMITEDEDITION' ? 'limited edition' : 'discontinued but still around'} size={medium} color={Colors.white}/>
       </View>
-      <View style={{flex:1,alignItems:'center',justifyContent:'space-around',flexDirection:'row',marginTop:20}}>
-        <TouchableOpacity style={{marginLeft:40}} onPress={props.onMinusPress}>
-          <Ionicons name="ios-remove-circle-outline" size={45} color={Colors.white} style={{marginHorizontal:20,marginBottom:12}}/>
-        </TouchableOpacity>
-        <FontMatilde text={props.inventoryCount} size={100} color={Colors.white}/>
-        <TouchableOpacity style={{marginRight:40}} onPress={props.onAddPress}>
-          <Ionicons name="ios-add-circle-outline" size={45} color={Colors.white} style={{marginHorizontal:20,marginBottom:12}}/>
-        </TouchableOpacity>
-      </View>
+      {
+        props.distributorId ?
+        <View style={{flex:1,alignItems:'center',justifyContent:'space-around',flexDirection:'row',marginTop:20}}>
+          <TouchableOpacity style={{marginLeft:40}} onPress={props.onMinusPress}>
+            <Ionicons name="ios-remove-circle-outline" size={45} color={Colors.white} style={{marginHorizontal:20,marginBottom:12}}/>
+          </TouchableOpacity>
+          <FontMatilde text={props.inventoryCount} size={100} color={Colors.white}/>
+          <TouchableOpacity style={{marginRight:40}} onPress={props.onAddPress}>
+            <Ionicons name="ios-add-circle-outline" size={45} color={Colors.white} style={{marginHorizontal:20,marginBottom:12}}/>
+          </TouchableOpacity>
+        </View> : null
+      }
       <View style={{...Views.middle,marginTop:20}}>
         <FontPoiret text={props.name.toUpperCase()} size={large} color={Colors.white}/>
       </View>
@@ -69,7 +73,7 @@ class LipColors extends Component {
     modalContent: {},
     user: this.props.user,
     colors: null,
-    shades: [],
+    colorIds: [],
     colorsByFamily: null,
     isListReady: false,
     DistributorId: this.props.user.distributorx ? this.props.user.distributorx.id : null
@@ -89,7 +93,30 @@ class LipColors extends Component {
     if (newProps && newProps.getColorsAndInventories && newProps.getColorsAndInventories.allColors) {
       if (newProps.getColorsAndInventories.allColors !== this.state.colors) {
         let colors = newProps.getColorsAndInventories.allColors
-        let shades = []
+        let colorIds = []
+        // colors.forEach(color => {
+        //   this.setState({
+        //     [`${color.id}`]:{
+        //       ...color,
+        //       inventory:{
+        //         id: color.inventoriesx.length > 0 ? color.inventoriesx[0].id : null,
+        //         count: color.inventoriesx.length > 0 ? color.inventoriesx[0].count : 0
+        //       }}
+        //   },()=>{
+        //     colorIds.push(color.id)
+        //   })
+        // })
+
+        let neutralsColorIds = []
+        let redsColorIds = []
+        let pinksColorIds = []
+        let boldPinksColorIds = []
+        let brownsColorIds = []
+        let purplesColorIds = []
+        let berriesColorIds = []
+        let orangesColorIds = []
+        let colorIdsCatcher = []
+
         colors.forEach(color => {
           this.setState({
             [`${color.id}`]:{
@@ -99,9 +126,37 @@ class LipColors extends Component {
                 count: color.inventoriesx.length > 0 ? color.inventoriesx[0].count : 0
               }}
           },()=>{
-            shades.push(color.id)
+            switch (color.family) {
+              case "NEUTRALS":
+                neutralsColorIds.push(color.id)
+                break;
+                case "REDS":
+                  redsColorIds.push(color.id)
+                  break;
+                  case "PINKS":
+                    pinksColorIds.push(color.id)
+                    break;
+                    case "BOLDPINKS":
+                      boldPinksColorIds.push(color.id)
+                      break;
+                      case "BROWNS":
+                        brownsColorIds.push(color.id)
+                        break;
+                        case "PURPLES":
+                          purplesColorIds.push(color.id)
+                          break;
+                          case "BERRIES":
+                            berriesColorIds.push(color.id)
+                            break;
+                            case "ORANGES":
+                              orangesColorIds.push(color.id)
+                              break;
+                            default:
+                              colorIdsCatcher.push(color.id)
+            }
           })
         })
+
         // let colorsByFamily = {
         //   neutrals:colors.filter(color => color.family === "NEUTRALS"),
         //   reds:colors.filter(color => color.family === "REDS"),
@@ -112,10 +167,21 @@ class LipColors extends Component {
         //   berries:colors.filter(color => color.family === "BERRIES"),
         //   oranges:colors.filter(color => color.family === "ORANGES")
         // }
-        this.setState({shades,colors},()=>{
-          this.setState({isListReady:true},()=>{
-            // console.log('test: ',this.state[`${this.state.shades[0]}`]);
-          })
+        this.setState({
+          colors,
+          neutralsColorIds,
+          redsColorIds,
+          pinksColorIds,
+          boldPinksColorIds,
+          brownsColorIds,
+          purplesColorIds,
+          berriesColorIds,
+          orangesColorIds,
+          colorIdsCatcher
+        },()=>{
+          setTimeout(()=>{
+            this.setState({isListReady:true});
+          },2000)
         })
       }
     }
@@ -148,17 +214,29 @@ class LipColors extends Component {
     )
   }
 
-  renderColorsList(){
+  openError(errText){
+    this.setState({isModalOpen:false},()=>{
+      setTimeout(()=>{
+        this.showModal('err','Lip Colors',errText)
+      },700)
+    })
+  }
+
+  renderColorsList(colorIds){
+    let { DistributorId } = this.state
     if (!this.state.isListReady) {
       return (
-        <Text>Loading...</Text>
+        <DotsLoader
+          size={15}
+          color={Colors.pink}
+          frequency={5000}/>
       )
     } else {
-      return this.state.shades.map(colorId => {
+      return colorIds.map(colorId => {
         let color = this.state[`${colorId}`]
         let { id,count } = color.inventory
         return <ColorCard
-          key={color.id} family={color.family} tone={color.tone} name={color.name} rgb={color.rgb ? `rgb(${color.rgb})` : Colors.purpleText}
+          key={color.id} family={color.family} tone={color.tone} name={color.name} rgb={color.rgb ? `rgb(${color.rgb})` : Colors.purpleText} distributorId={DistributorId}
           finish={color.finish} status={color.status} inventoryCount={count} inventoryId={id}
           onAddPress={() => this.checkIfInventoryExists(id,color.id,count,'+')}
           onMinusPress={() => this.checkIfInventoryExists(id,color.id,count,'-')}/>
@@ -171,11 +249,36 @@ class LipColors extends Component {
   }
 
   renderMainContent(){
+    let {
+      neutralsColorIds,
+      redsColorIds,
+      pinksColorIds,
+      boldPinksColorIds,
+      brownsColorIds,
+      purplesColorIds,
+      berriesColorIds,
+      orangesColorIds
+    } = this.state
     return (
       <View style={{flex:1}}>
         <ScrollView
-          contentContainerStyle={{marginBottom:56}}>
-            {this.renderColorsList()}
+          contentContainerStyle={{marginBottom:56,justifyContent:'center',alignItems:'center'}}>
+            <FontPoiret text="neutrals" size={xlarge} color={Colors.blue} style={{marginTop:10}}/>
+            {this.renderColorsList(neutralsColorIds)}
+            <FontPoiret text="reds" size={xlarge} color={Colors.blue} style={{marginTop:40}}/>
+            {this.renderColorsList(redsColorIds)}
+            <FontPoiret text="pinks" size={xlarge} color={Colors.blue} style={{marginTop:40}}/>
+            {this.renderColorsList(pinksColorIds)}
+            <FontPoiret text="bold pinks" size={xlarge} color={Colors.blue} style={{marginTop:40}}/>
+            {this.renderColorsList(boldPinksColorIds)}
+            <FontPoiret text="browns" size={xlarge} color={Colors.blue} style={{marginTop:40}}/>
+            {this.renderColorsList(brownsColorIds)}
+            <FontPoiret text="purples" size={xlarge} color={Colors.blue} style={{marginTop:40}}/>
+            {this.renderColorsList(purplesColorIds)}
+            <FontPoiret text="berries" size={xlarge} color={Colors.blue} style={{marginTop:40}}/>
+            {this.renderColorsList(berriesColorIds)}
+            <FontPoiret text="oranges" size={xlarge} color={Colors.blue} style={{marginTop:40}}/>
+            {this.renderColorsList(orangesColorIds)}
         </ScrollView>
       </View>
     )
@@ -192,6 +295,7 @@ class LipColors extends Component {
   }
 
   checkIfInventoryExists(InventoryId,ColorId,InventoryCount,op){
+    this.showModal('processing')
     let { DistributorId } = this.state
     if (InventoryId) {
       this.updateInventory(InventoryId,InventoryCount,op)
@@ -216,16 +320,19 @@ class LipColors extends Component {
               ...this.state[`${ColorId}`],
               inventory: {id,count}
             }
+          },()=>{
+            setTimeout(()=>{
+              this.setState({isModalOpen:false})
+            },1000)
           })
         } else {
-          this.showModal('err','Lip Colors','updating your inventory')
+          this.openError(inventoryError)
         }
       }).catch( e => {
-        console.log('error creating inventory',e.message);
-        this.showModal('err','Lip Colors','updating your inventory')
+        this.openError(inventoryError)
       })
     } else {
-      this.showModal('err','Lip Colors','updating your inventory')
+      this.openError(inventoryError)
     }
   }
 
@@ -244,16 +351,19 @@ class LipColors extends Component {
               ...this.state[`${InventoryId}`],
               inventory: {id,count}
             }
+          },()=>{
+            setTimeout(()=>{
+              this.setState({isModalOpen:false})
+            },1000)
           })
         } else {
-          this.showModal('err','Lip Colors','updating your inventory')
+          this.openError(inventoryError)
         }
       }).catch( e => {
-        console.log('error updating inventory',e.message);
-        this.showModal('err','Lip Colors','updating your inventory')
+        this.openError(inventoryError)
       })
     } else {
-      this.showModal('err','Lip Colors','updating your inventory')
+      this.openError(inventoryError)
     }
   }
 
