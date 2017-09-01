ffd22b21835d2c2f1094fbb07aae629b735339c9
Chris Castro <home@castrombpro.home>
2017-09-01 03:16:57 -0400

    Fix Insufficient Permissions Error

--- api/ApolloClientRT.js
+++ api/ApolloClientRT.js
@@ -9,19 +9,6 @@ import { PROJECT_ID } from 'react-native-dotenv'
 import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 import { ApolloClient,createNetworkInterface } from 'react-apollo'
 
-const getToken = () => {
-  AsyncStorage.getItem('gcToken').then(
-    gcToken => {
-      console.log(gcToken);
-      let token = gcToken ? `Bearer ${gcToken}` : null
-      return token
-    },
-    failure => {
-      Alert.alert('ERROR: ', failure.message)
-    }
-  )
-}
-
 const networkInterface = createNetworkInterface({
   uri: `https://api.graph.cool/simple/v1/${PROJECT_ID}`,
   opts: {
@@ -29,13 +16,11 @@ const networkInterface = createNetworkInterface({
   }
 })
 
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MDY1ODAzNDUsImlhdCI6MTUwMzk4ODM0NSwicHJvamVjdElkIjoiY2o1YXllNzN2Z3ZnYjAxMzNnOW41N2V0biIsInVzZXJJZCI6ImNqNnZwenp4bGl6NXgwMTgzNGFhZ2M2aWUiLCJtb2RlbE5hbWUiOiJVc2VyIn0.wYs8Ww-olrX3Y0CoRlYr0DBk-DeSSpV3Aypjs2_88Ls"
-
 const wsClient = new SubscriptionClient(`wss://subscriptions.us-west-2.graph.cool/v1/${PROJECT_ID}`, {
   reconnect: true,
   timeout: 20000,
   connectionParams: {
-    Authorization: token
+    Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MDY1ODAzNDUsImlhdCI6MTUwMzk4ODM0NSwicHJvamVjdElkIjoiY2o1YXllNzN2Z3ZnYjAxMzNnOW41N2V0biIsInVzZXJJZCI6ImNqNnZwenp4bGl6NXgwMTgzNGFhZ2M2aWUiLCJtb2RlbE5hbWUiOiJVc2VyIn0.wYs8Ww-olrX3Y0CoRlYr0DBk-DeSSpV3Aypjs2_88Ls"
   }
 })
 
@@ -43,16 +28,6 @@ const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
   networkInterface,
   wsClient
 )
-
-// NOT SUPPORTED YET
-// wsClient.use([
-//   {
-//     applyMiddleware(operationOptions, next) {
-//       operationOptions["Authorization"] = getToken()
-//       next()
-//     }
-//   }
-// ])
 
 networkInterface.use([{
   applyMiddleware(req, next) {
