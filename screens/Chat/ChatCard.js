7109e04d0085116a4174c7e9804ffb0f3c294cc5
Chris Castro <home@castrombpro.home>
2017-09-16 12:42:32 -0400

    Format ChatCardLayout

--- screens/Chat/ChatCard.js
+++ screens/Chat/ChatCard.js
@@ -21,11 +21,11 @@ import { getDimensions,clipText } from '../../utils/Helpers'
 // CONSTs
 const small = Texts.small.fontSize
 const medium = Texts.medium.fontSize
+const size = 90
 const screen = getDimensions()
 
-const ChatCard = props => {
-
-  let size = 90
+const ChatCardLayout = props => {
+  let { approved,uri,chatTitle,chatSubTitle,message,date,line1,line2 } = props
   let width = screen.width*.95
   let cardLeft = {width:size,height:size}
   let cardRight = {height:size,paddingHorizontal:10,paddingVertical:5}
@@ -36,6 +36,42 @@ const ChatCard = props => {
   let cardStyle = {
     width,flexDirection:'row',backgroundColor:Colors.pinkly,borderRadius:12,marginVertical:6
   }
+
+  if (approved) {
+    return (
+      <View style={cardStyle}>
+        <View style={cardLeft}>
+          <Image source={{uri}} style={imgSize}/>
+        </View>
+        <View style={cardRight}>
+          <FontPoiret text={clipText(chatTitle,17)} size={medium} color={Colors.white}/>
+          {
+            chatSubTitle ?
+            <FontPoiret text={clipText(chatSubTitle,20)} size={small} color={Colors.white}/> :
+              null
+          }
+          <FontPoiret text={message} size={small} color={Colors.white}/>
+          <FontPoiret text={moment(date).fromNow()} size={small} color={Colors.white}/>
+        </View>
+      </View>
+    )
+  } else {
+    return (
+      <View style={cardStyle}>
+        <View style={cardLeft}>
+          <Image source={require('../../assets/images/avatar.png')} style={imgSize}/>
+        </View>
+        <View style={noExist}>
+          <FontPoiret text="show lock" size={medium} color={Colors.white}/>
+          <FontPoiret text={line1} size={medium} color={Colors.white}/>
+          <FontPoiret text={line2} size={medium} color={Colors.white}/>
+        </View>
+      </View>
+    )
+  }
+}
+
+const ChatCard = props => {
 
   let { chat,userType,viewersStatus } = props
   // console.log('props on ChatCard',props);
@@ -58,7 +94,7 @@ const ChatCard = props => {
   let name = `${fbkFirstName || ''} ${fbkLastName || ''}`
 
   let chatTitle = alias ? alias : userType === 'SHOPPER' ? bizName : name
-  let chatSubTitle = `by ${name}`
+  let chatSubTitle = status ? `by ${name}` : null
   let message = chat.messages.length > 0 ? chat.messages[0].text : 'no chat history'
 
   let uri = logoUri && logoUri.length > 8 ? logoUri : `https://graph.facebook.com/${fbkUserId}/picture?width=${size}&height=${size}`
@@ -68,106 +104,32 @@ const ChatCard = props => {
       if (userType === 'DIST') {
           if (viewersStatus) {
             // approved DIST viewing an approved DIST
-            return (
-              <View style={cardStyle}>
-                <View style={cardLeft}>
-                  <Image source={{uri}} style={imgSize}/>
-                </View>
-                <View style={cardRight}>
-                  <FontPoiret text={clipText(chatTitle,17)} size={medium} color={Colors.white}/>
-                  <FontPoiret text={clipText(chatSubTitle,20)} size={small} color={Colors.white}/>
-                  <FontPoiret text={message} size={small} color={Colors.white}/>
-                  <FontPoiret text={moment(date).fromNow()} size={small} color={Colors.white}/>
-                </View>
-              </View>
-            )
+            return <ChatCardLayout approved={true} uri={uri} chatTitle={chatTitle} chatSubTitle={chatSubTitle} message={message} date={date}/>
           } else {
             // unapproved DIST viewing an approved DIST
-            return (
-              <View style={cardStyle}>
-                <View style={cardLeft}>
-                  <Image source={require('../../assets/images/avatar.png')} style={imgSize}/>
-                </View>
-                <View style={noExist}>
-                  <FontPoiret text="show lock" size={medium} color={Colors.white}/>
-                </View>
-              </View>
-            )
+            return <ChatCardLayout approved={false} line1="your fellow distributors are" line2="waiting for you to get approved"/>
           }
       } else {
         // shopper viewing an approved DIST [tested,passed]
-        return (
-          <View style={cardStyle}>
-            <View style={cardLeft}>
-              <Image source={{uri}} style={imgSize}/>
-            </View>
-            <View style={cardRight}>
-              <FontPoiret text={clipText(chatTitle,17)} size={medium} color={Colors.white}/>
-              <FontPoiret text={message} size={small} color={Colors.white}/>
-              <FontPoiret text={moment(date).fromNow()} size={small} color={Colors.white}/>
-            </View>
-          </View>
-        )
+        return <ChatCardLayout approved={true} uri={uri} chatTitle={chatTitle} chatSubTitle={chatSubTitle} message={message} date={date}/>
       }
   } else {
     if (status === false) {
       // viewing unapproved DIST [tested,passed]
-      return (
-        <View style={cardStyle}>
-          <View style={cardLeft}>
-            <Image source={require('../../assets/images/avatar.png')} style={imgSize}/>
-          </View>
-          <View style={noExist}>
-            <FontPoiret text="distributor exists but hasn't" size={medium} color={Colors.white}/>
-            <FontPoiret text="been approved yet" size={medium} color={Colors.white}/>
-          </View>
-        </View>
-      )
+      return <ChatCardLayout approved={false} line1="distributor exists but has" line2="not been approved yet"/>
     } else {
       // viewing shoppers
       if (userType === 'DIST') {
           if (viewersStatus) {
             // approved DIST viewing a shopper [tested,passed]
-            return (
-              <View style={cardStyle}>
-                <View style={cardLeft}>
-                  <Image source={{uri}} style={imgSize}/>
-                </View>
-                <View style={cardRight}>
-                  <FontPoiret text={clipText(chatTitle,17)} size={medium} color={Colors.white}/>
-                  <FontPoiret text={message} size={small} color={Colors.white}/>
-                  <FontPoiret text={moment(date).fromNow()} size={small} color={Colors.white}/>
-                </View>
-              </View>
-            )
+            return <ChatCardLayout approved={true} uri={uri} chatTitle={chatTitle} chatSubTitle={chatSubTitle} message={message} date={date}/>
           } else {
             // unapproved DIST viewing a shopper [tested,passed]
-            return (
-              <View style={cardStyle}>
-                <View style={cardLeft}>
-                  <Image source={require('../../assets/images/avatar.png')} style={imgSize}/>
-                </View>
-                <View style={noExist}>
-                  <FontPoiret text="show lock" size={medium} color={Colors.white}/>
-                  <FontPoiret text="this shopper is waiting for you to get approved" size={medium} color={Colors.white}/>
-                </View>
-              </View>
-            )
+            return <ChatCardLayout approved={false} line1="this shopper is waiting" line2="for you to get approved"/>
           }
       } else {
         // shopper viewing shopper
-        return (
-          <View style={cardStyle}>
-            <View style={cardLeft}>
-              <Image source={{uri}} style={imgSize}/>
-            </View>
-            <View style={cardRight}>
-              <FontPoiret text={clipText(chatTitle,17)} size={medium} color={Colors.white}/>
-              <FontPoiret text={message} size={small} color={Colors.white}/>
-              <FontPoiret text={moment(date).fromNow()} size={small} color={Colors.white}/>
-            </View>
-          </View>
-        )
+        return <ChatCardLayout approved={true} uri={uri} chatTitle={chatTitle} chatSubTitle={chatSubTitle} message={message} date={date}/>
       }
     }
   }
