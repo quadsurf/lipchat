3b412898313e4e3dac942f03ca2e08ed6e8c210d
Chris Castro <quadsurf@gmail.com>
2018-04-08 07:22:16 -0400

    Improve Tips Text

--- config/Defaults.js
+++ config/Defaults.js
@@ -14,7 +14,7 @@ export const tips = `We live by this quote:
 
 "Continuous Improvement is better than Delayed Perfection".
 
-This screen processes an insane amount of data in real-time, which causes an inconvenient slow-down in performance, which means the app will be slow to react to your finger taps/swipes. We apologize for this, and ask for your patience. While we will be improving/optimizing these features over the next several months, in the meantime we recommend the following practices to improve your experience for now:
+This screen processes an insane amount of data in real-time, which causes an inconvenient slow-down in performance, which means the app will be slow to react to your finger taps/swipes. We apologize for this, and ask for your patience while we seek to improve/optimize these features. In the meantime though, we recommend the following practices to improve your experience for now:
 
 1. tap and/or swipe slowly
 
